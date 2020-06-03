14b3ec8514f2a81ca5bd1b75891f1b9d07cf54da
Anastasia <66080283+anastasia-naumova@users.noreply.github.com>
2020-06-03 19:21:40 +0300

    Code changas
    The code was broken down by function. Imported module time.

--- version of code_1.0.py
+++ version of code_1.0.py
@@ -1,34 +1,86 @@
 import psycopg2
+import time
 
 
-conn = psycopg2.connect(database="demo", user="annaum", password="****", host="192.168.1.***", port="5432")
-cursor = conn.cursor()
+time_start = time.time()
 
-cursor.execute("select distinct table_name from target_bookings.meta_table mt ")
-results = cursor.fetchall()
 
-root_table_name = []
-for i in results:
-    root_table_name.append(i[0])
-print(root_table_name)
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port)
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Error while creating PostgreSQL table", error)
+    return connection
 
-cursor.execute("select b.table_name, count(*) from bookings.meta_table b left join target_bookings.meta_table tb "
-               "on b.insert_dttm = tb.cut_value where insert_dttm > '03-08-2017 07:00:00' group by b.table_name")
-results2 = cursor.fetchall()
-print(results2)
 
-if len(root_table_name) == len(results2):
-    cursor.execute("delete from target_bookings.results_table")
-    cursor.execute("insert into target_bookings.results_table select f.flight_id, flight_no, departure_airport,"
-                   " actual_departure, arrival_airport, actual_arrival, tf.ticket_no, fare_conditions, "
-                   "passenger_id from bookings.flights f join bookings.ticket_flights tf on f.flight_id = tf.flight_id "
-                   "join bookings.tickets t on tf.ticket_no = t.ticket_no "
-                   "where f.arrival_airport = 'ROV' order by f.flight_id")
-    cursor.execute("update target_bookings.meta_table set dataflow_dttm = dataflow_dttm + interval '1 day', "
-                   "cut_value = (select max(insert_dttm ) from bookings.meta_table "
-                   "where target_bookings.meta_table.table_name = bookings.meta_table.table_name)")
-    
-    conn.commit()
+def execute_sql_resalt(connection, sql_select_query):
+    cursor = connection.cursor()
+    cursor.execute(sql_select_query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 
-cursor.close()
-conn.close()
+
+def execute_sql_query(connection, sql_querty):
+    cursor = connection.cursor()
+    cursor.execute(sql_querty)
+    connection.commit()
+    cursor.close()
+
+
+def create_resalt_table(connection, name_in_meta_table, table_name_count_record, sql_delete_querty, sql_insert_querty,
+                        sql_update_querty):
+    cursor = connection.cursor()
+    if execute_sql_resalt(connection, name_in_meta_table) == execute_sql_resalt(connection, table_name_count_record):
+        execute_sql_query(connection, sql_delete_querty)
+        execute_sql_query(connection, sql_insert_querty)
+        execute_sql_query(connection, sql_update_querty)
+    else:
+        time_now = time.time()
+        if time_now - time_start < 30:
+            time.sleep(3)
+            create_resalt_table(connection, name_in_meta_table, table_name_count_record, sql_delete_querty,
+                                sql_insert_querty, sql_update_querty)
+        else:
+            try:
+                create_resalt_table(connection, name_in_meta_table, table_name_count_record, sql_delete_querty,
+                                    sql_insert_querty, sql_update_querty)
+            except:
+                raise TimeoutError('Время ожидания истекло')
+
+connection = create_connection("demo", "annaum", "*****", "192.168.1.***", "5432")
+
+name_in_meta_table = "select table_name from target_bookings.last_taken_data where jobe_name = 'a001'"
+
+table_name_count_record = "select us.table_name from bookings.update_status us " \
+                          "where us.table_name = 'flights' and insert_dttm > (select cut_value " \
+                          "from target_bookings.last_taken_data where jobe_name = 'a001' and table_name = 'flights') " \
+                          "union select us.table_name from bookings.update_status us " \
+                          "where us.table_name = 'ticket_flights' and insert_dttm > (select cut_value " \
+                          "from target_bookings.last_taken_data where jobe_name = 'a001' and " \
+                          "table_name = 'ticket_flights') union select us.table_name from bookings.update_status us " \
+                          " where us.table_name = 'tickets' and insert_dttm > (select cut_value " \
+                          "from target_bookings.last_taken_data where jobe_name = 'a001' and table_name = 'tickets')"
+
+sql_delete_querty = "delete from target_bookings.results_table_a001"
+
+sql_insert_querty = "insert into target_bookings.results_table_a001 select f.flight_id, flight_no, departure_airport," \
+                    "actual_departure, arrival_airport, actual_arrival, tf.ticket_no, fare_conditions, " \
+                    "passenger_id from bookings.flights f join bookings.ticket_flights tf " \
+                    "on f.flight_id = tf.flight_id join bookings.tickets t on tf.ticket_no = t.ticket_no " \
+                    "where f.arrival_airport = 'ROV'"
+
+sql_update_querty = "update target5еа чрнисо7 гс7рг7мпгт _bookings.last_taken_data set dataflow_dttm = dataflow_dttm + interval '1 day', " \
+                    "cut_value = (select max(insert_dttm ) from bookings.update_status " \
+                    "where target_bookings.last_taken_data.table_name = bookings.update_status.table_name) " \
+                    "where jobe_name = 'a001'"
+
+create_resalt_table(connection, name_in_meta_table, table_name_count_record, sql_delete_querty, sql_insert_querty,
+                    sql_update_querty)
+connection.close()
